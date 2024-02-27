18e21326e511c6c1f298d90bb456e29a13c3ef88
Ap1kkk <ApikMaster2@yandex.ru>
2024-02-27 18:07:15 +0300

    init

---
+++ main.py
@@ -0,0 +1,73 @@
+import socket
+from pythonping import ping
+import matplotlib.pyplot as plt
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_ip(site_url):
+    try:
+        python_ip = socket.gethostbyname(site_url)
+        return python_ip
+    except socket.error as err:
+        print(f"Error: {err}")
+        return None
+
+
+def ping_website(site_url):
+    try:
+        response = ping(site_url, size=4, count=11)
+        print(response)
+        avg_rtt = response.rtt_avg_ms
+        rtt_times = [resp.time_elapsed_ms for resp in response]
+        return avg_rtt, rtt_times
+    except Exception as err:
+        print(f"Error: {err}")
+        return None, None
+
+
+def print_RTT(rtt_times):
+    plt.hist(rtt_times, bins='auto', density=True)
+    plt.xlabel('RTT (ms)')
+    plt.ylabel('Плотность вероятности')
+    plt.title('Распределение RTT')
+    plt.grid(True)
+    plt.show()
+
+
+def get_exchange_rate(site_url):
+    response = requests.get(site_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    aud_element = soup.find('a', {'title': 'Австралийский доллар'})
+
+    exchange_rate = aud_element.find_all('div')
+    code = exchange_rate[0].text.strip()
+    denomination = exchange_rate[1].text.strip()
+    currency = exchange_rate[2].text.strip()
+    value = exchange_rate[3].text.strip()
+    change = exchange_rate[4].text.strip()
+    percent = exchange_rate[5].text.strip()
+
+    return code, denomination, currency, value, change, percent
+
+
+url_to_ping = "www.python.org"
+url_to_parse = "https://finance.rambler.ru/currencies/"
+
+ip = get_ip(url_to_ping)
+print(f"The IP address of {url_to_ping} is {ip}")
+
+avgRTT, timesRTT = ping_website(url_to_ping)
+print(f"Среднее арифметическое RTT = {avgRTT}")
+
+print_RTT(timesRTT)
+
+
+values = get_exchange_rate(url_to_parse)
+print(f"\nКод: {values[0]}")
+print(f"Номинал: {values[1]}")
+print(f"Валюта: {values[2]}")
+print(f"Курс ЦБ: {values[3]}")
+print(f"Изменение: {values[4]}")
+print(f"%: {values[5]}")
